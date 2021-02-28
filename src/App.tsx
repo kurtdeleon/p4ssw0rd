17f1ff6c539d4e2ea892dfa9f0954ad08b68fff4
kurtdeleon <deleon.ksa@gmail.com>
2021-02-28 16:04:10 +0800

    Fixed visual issues, removed themer

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,23 @@
-import React, { useState, useCallback } from "react";
+import React from "react";
 import PasswordGenerator from "src/components/PasswordGenerator";
 import { ReactComponent as Github } from "src/assets/github.svg";
 import "./App.scss";
 
 const App: React.FC = () => {
-  const [darkMode, setDarkMode] = useState<boolean>(true);
-
-  const toggleTheme = useCallback(() => {
-    setDarkMode((darkMode) => !darkMode);
-  }, [darkMode]);
-
   return (
     <div id="app">
       <div id="title-bar">
         <span className="code">p4ssw0rd</span>
-        {/* <input id="chck" type="checkbox" onClick={toggleTheme} />
-        <label htmlFor="chck" className="check-trail">
-          <span className="check-handler"></span>
-        </label> */}
       </div>
       <PasswordGenerator />
       <div id="footer">
-        <Github />
+        <a
+          href="https://github.com/kurtdeleon/p4ssw0rd"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Github />
+        </a>
       </div>
     </div>
   );
